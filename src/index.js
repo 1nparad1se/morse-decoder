e685ef0d43349a61dfeb3a52534b5c0c50558b1a
Jeyhun <inparadisejeyhun@gmail.com>
2024-08-11 02:29:51 +0300

    feat: relis func decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const total = [];
+    const finish = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        total.push(expr.slice(i, i + 10));
+    }
+
+
+    for (let i = 0; i < total.length; i++) {
+        let letter = [];
+        for (let j = 0; j < 10; j += 2) {
+            if (`${total[i][j]}${total[i][j + 1]}` === '10') {
+                letter.push('.')
+            } else if (`${total[i][j]}${total[i][j + 1]}` === '11') {
+                letter.push('-')
+            }
+        }
+        finish.push(letter.join());
+    }
+
+
+    let text = ''
+    for (let i = 0; i < finish.length; i++) {
+        if (!finish[i]) {
+            text += ' '
+        } else {
+            text += MORSE_TABLE[finish[i].split(',').join('')]
+        }
+    }
+
+    return text
 }
 
 module.exports = {
